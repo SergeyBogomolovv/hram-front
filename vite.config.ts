728aa9de26ad13043a7787b90b35bf36297df59c
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-11-26 21:18:46 +0300

    feat: add postcss and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {},
+  },
   resolve: {
     alias: {
       '@': '/src',
     },
   },
-})
+});
